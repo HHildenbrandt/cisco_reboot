025e4fc695705712956d4b633d9e477df7c395ef
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2022-02-19 14:34:46 +0100

    created

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## dependencies
 ```bash
+# selenium webdriver firefox
 wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux32.tar.gz
 tar -x geckodriver -zf geckodriver-v0.30.0-linux32.tar.gz
 sudo mv geckodriver /usr/bin/
--- cisco-reboot.py
+++ cisco-reboot.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+
 # Selenium script to restart CISCO modem
 # Hanno 2022
 
+import sys
+import getopt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -8,24 +12,52 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 
-url = " http://192.168.178.1/"
 
-driver = webdriver.Firefox()
-driver.get(url + "login_zig.asp")
-driver.set_window_size(1024, 720)
+def main(argv):
+    url = " http://192.168.178.1/"
+    name = "hanno"
+    passw = "TRfrakt01CISC"
+    path = r"/usr/bin/firefox"
 
-driver.find_element(By.ID, "Zigloginnaam").click()
-driver.find_element(By.ID, "Zigloginnaam").send_keys("hanno")
-driver.find_element(By.ID, "Zigpassword").send_keys("TRfrakt01CISC")
-driver.find_element(By.LINK_TEXT, "Inloggen >").click()
+    try:
+        opts, args = getopt.getopt(argv, '', ["hot","headless"])
+    except getopt.GetoptError:
+        print('cisco-reboot.py [--hot] [--headless]')
+        sys.exit(2)
 
-driver.get(url + "Devicerestart.asp")
-#driver.find_element(By.CSS_SELECTOR, "a > img").click()
-#driver.find_element(By.CSS_SELECTOR, ".MainTabL23").click()
-#driver.find_element(By.CSS_SELECTOR, ".SubTabL23").click()
-driver.find_element(By.NAME, "mtenRestore").click()
-assert driver.switch_to.alert.text == "Select OK to restart the device."
-driver.switch_to.alert.dismiss()
-  
-#driver.quit()
+    hot, headless = False, False    
+    for opt, arg in opts:
+        if opt == "--hot":
+            hot = True
+        if opt == "--headless":
+            headless = True
+
+    options = Options()
+    options.headless = headless
+    driver = webdriver.Firefox(options=options)
+        
+    driver.get(url + "login_zig.asp")
+    driver.set_window_size(1024, 720)
+
+    driver.find_element(By.ID, "Zigloginnaam").click()
+    driver.find_element(By.ID, "Zigloginnaam").send_keys(name)
+    driver.find_element(By.ID, "Zigpassword").send_keys(passw)
+    driver.find_element(By.LINK_TEXT, "Inloggen >").click()
+
+    driver.get(url + "Devicerestart.asp")
+    driver.find_element(By.NAME, "mtenRestore").click()
+    alert = WebDriverWait(driver, 5).until(expected_conditions.alert_is_present())
+    assert alert.text == "Select OK to restart the device."
+    if hot:
+        alert.accept()
+        print("Restart device confirmed.")
+    else:
+        alert.dismiss()
+        print("Restart device dismissed. Use --hot")
+    driver.quit()
+    print(hot, headless)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
